4a7a9e3025b89e778c6dd2fc079f10682e247e3e
getrod <kevinlouisjean@gmail.com>
2020-09-22 21:43:47 -0400

    added login requests

--- myftp.py
+++ myftp.py
@@ -13,9 +13,29 @@ controlSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     print("Connecting to " + controlServerName + "...")
     controlSocket.connect((controlServerName, controlServerPort))
+    print(controlSocket.recv(1024))
 except socket.error:
     print("Could not connect to " + controlServerName)
     sys.exit()
 
 print("Connection Successful")
+
+# Request username
+username = raw_input("Username: ")
+controlSocket.send("USER %s\r\n" % username)
+resp = controlSocket.recv(1024)
+replyCode = int(resp[0])
+if (replyCode != 2 and replyCode != 3):
+    print("Username was unsuccessful")
+    sys.exit()
+
+# Request Password
+password = raw_input("Password: ")
+controlSocket.send("PASS %s\r\n" % password)
+resp = controlSocket.recv(1024)
+replyCode = int(resp[0])
+if (replyCode != 2):
+    print("Password was unsuccessful")
+    sys.exit()
+
 controlSocket.close()
